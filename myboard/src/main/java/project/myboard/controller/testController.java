eb79c7c5e49b91335b9d108e23378faed3588cda
jkkim <jaekyeong930@gmail.com>
2024-09-29 16:01:14 +0900

    launch myboard with spring

---
+++ myboard/src/main/java/project/myboard/controller/testController.java
@@ -0,0 +1,13 @@
+package project.myboard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
